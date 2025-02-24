6828204ad2b6a81c8d063aaca8d3222cebf0522e
Naufal Alfa <108531196+AlfaNaufal@users.noreply.github.com>
2025-02-24 12:07:52 +0700

    First Commit

--- template.c
+++ template.c
@@ -1,15 +1,80 @@
 #include <graphics.h>
 #include <conio.h>
 #include <stdio.h>
+#include <time.h>
 
-#define SCREEN_WIDTH  640
-#define SCREEN_HEIGHT 480
+#define WIDTH  520
+#define HEIGHT 640
+#define SCREEN_WIDTH  280
+#define SCREEN_HEIGHT 500
 #define BLOCK_SIZE    20
+
+#define GRID_X 100     // Posisi awal grid di layar
+#define GRID_Y 0
+
+// // Bentuk Tetromino (T)
+// int tetromino[4][4] = {
+//     {0, 1, 0, 0},
+//     {0, 1, 0, 0},
+//     {1, 1, 0, 0},
+//     {0, 0, 0, 0}
+// };
+
+// // Fungsi untuk menggambar satu Tetromino di posisi (x, y)
+// void drawTetromino(int x, int y, int color) {
+//     setfillstyle(SOLID_FILL, color);
+    
+//     for (int i = 0; i < 4; i++) {
+//         for (int j = 0; j < 4; j++) {
+//             if (tetromino[i][j] == 1) {
+//                 bar(x + j * BLOCK_SIZE, y + i * BLOCK_SIZE, 
+//                     x + (j + 1) * BLOCK_SIZE, y + (i + 1) * BLOCK_SIZE);
+//             }
+//         }
+//     }
+// }
+
+typedef enum { I, O, T, L, J, S, Z } TetrominoType;
+
+// Koordinat relatif untuk setiap tetromino
+int offsets[7][4][2] = {
+    { {0,1}, {1,1}, {2,1}, {3,1} },  // I
+    { {0,0}, {1,0}, {0,1}, {1,1} },  // O
+    { {1,0}, {0,1}, {1,1}, {2,1} },  // T
+    { {2,0}, {0,1}, {1,1}, {2,1} },  // L
+    { {0,0}, {0,1}, {1,1}, {2,1} },  // J
+    { {1,0}, {2,0}, {0,1}, {1,1} },  // S
+    { {0,0}, {1,0}, {1,1}, {2,1} }   // Z
+};
+
+// Warna untuk setiap tetromino
+int tetrominoColors[7] = { CYAN, YELLOW, MAGENTA, WHITE, BLUE, GREEN, RED };
+
+// Fungsi untuk menggambar tetromino
+void drawTetromino(int x, int y, TetrominoType type) {
+    setfillstyle(SOLID_FILL, tetrominoColors[type]);
+    
+    for (int i = 0; i < 4; i++) {
+        int blockX = GRID_X + (x + offsets[type][i][0]) * BLOCK_SIZE;
+        int blockY = GRID_Y + (y + offsets[type][i][1]) * BLOCK_SIZE;
+        bar(blockX, blockY, blockX + BLOCK_SIZE, blockY + BLOCK_SIZE);
+    }
+}
+
+// Fungsi delay sederhana menggunakan clock
+void delayMillis(int milliseconds) {
+    clock_t start_time = clock();
+    while (clock() < start_time + milliseconds);
+}
+
 
 // Struktur untuk menyimpan data permainan
 typedef struct {
     int score;
     int level;
+    int fallSpeed = 500;
+    int tetrominoX = 4;  // Posisi grid (bukan piksel)
+    int tetrominoY = 0;
     int board[20][10];
     int currentBlock[4][4];
     int nextBlock[4][4];
@@ -18,15 +83,30 @@ typedef struct {
 } GameState;
 
 GameState game;
+TetrominoType currentTetronimo;
 
-// Inisialisasi grafikoid initGraphics() {
+void initGraphics() {
     int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
+    initgraph(&gd, &gm, NULL);
 }
 
 // Menggambar papan permainan
 void drawBoard() {
     // TODO: Implementasi menggambar papan dan blok
+    for (int x = 0; x <= WIDTH; x += BLOCK_SIZE) {
+        for (int y = 0; y <= HEIGHT; y += BLOCK_SIZE) {
+            // rectangle(x, y, x + BLOCK_SIZE, y + BLOCK_SIZE);
+        }
+    }
+}
+
+void drawScreen(){
+    for (int x = 100; x <= SCREEN_WIDTH; x += BLOCK_SIZE) {
+        for (int y = 0; y <= SCREEN_HEIGHT; y += BLOCK_SIZE) {
+            rectangle(x, y, x + BLOCK_SIZE, y + BLOCK_SIZE);
+            setbkcolor(GREEN);
+        }
+    }
 }
 
 // Menggambar skor dan level
@@ -89,9 +169,44 @@ void gameLoop() {
     }
 }
 
+// void screenRefresh(GameState game, TetrominoType currentTetronimo){
+
+//     while(true){
+
+//         cleardevice();
+        
+//         drawTetromino(game.tetrominoX, game.tetrominoY, currentTetronimo);
+
+//         game.tetrominoY++;
+//         delayMillis(game.fallSpeed);
+
+        
+//     }
+// }
+
 int main() {
     initGraphics();
-    gameLoop();
+    // drawBoard();
+    drawScreen();
+    // drawTetromino(140, 80, BLUE);
+    // gameLoop();
+
+    // drawTetromino(GRID_X, GRID_Y, T);
+    currentTetronimo = T;
+
+    while(1){
+
+        cleardevice();
+        
+        drawTetromino(game.tetrominoX, game.tetrominoY, currentTetronimo);
+
+        game.tetrominoY++;
+        delayMillis(game.fallSpeed);
+
+        
+    }
+
+    getch();
     closegraph();
     return 0;
 }
